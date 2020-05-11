b0a0e6f10dc4aabc221fa069d159bc9667b9263c
bpawluczuk <b.pawluczuk@companion.pl>
2020-05-11 18:13:45 +0200

    Init test

---
+++ src/test/java/HashTableTDDTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HashTableTDDTest {
+
+    @Test
+    public void testHashTable(){
+        assertEquals(1,1);
+    }
+}
