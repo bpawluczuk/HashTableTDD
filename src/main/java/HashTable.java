e0a01fc45fb99f37938153dc4648af316e2482b8
bpawluczuk <b.pawluczuk@companion.pl>
2020-05-12 18:56:55 +0200

    refactor case 2

--- src/main/java/HashTable.java
+++ src/main/java/HashTable.java
@@ -1,15 +1,20 @@
 public class HashTable<K, V> {
     private int size;
+    private int numBuckets;
     public HashTable() {
         size = 0;
+        numBuckets = 10;
     }
     public boolean isEmpty() {
         return size == 0;
     }
     public int getBucketIndex(K key){
-        return 0;
+        int hashCode = key.hashCode();
+        return (int) hashCode % numBuckets;
     };
 }
+
+
 
 
 
